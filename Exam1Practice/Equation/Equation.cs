682c2457de9a59d882869257e1f27fc575125480
Jordan Mosley <jordantmosley@ou.edu>
2020-02-18 01:12:04 -0600

    .

---
+++ Exam1Practice/Equation/Equation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equation
+{
+    class Equation
+    {
+        public double Left { get; set; }
+        public double Right { get; set; }
+
+        public Equation()
+        {
+            Left = 0.0;
+            Right = 0.0;
+        }
+
+        public Equation(double left, double right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public double Add()
+        {
+            double answer = Left + Right;
+            return answer;
+
+        }
+
+        public double Subtract()
+        {
+            double answer = Left - Right;
+            return answer;
+
+        }
+
+        public double Multiply()
+        {
+            double answer = Left * Right;
+            return answer;
+        }
+
+        public double LeftToThePower(int power)
+        {
+            double answer = Math.Pow(Left, power);
+            return answer;
+        }
+
+        public double RightToThePower(int power)
+        {
+            double answer = Math.Pow(Right, power);
+            return answer;
+        }
+    }
+}
---
+++ Exam1Practice/Equation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation test = new Equation();
+
+            Console.WriteLine("Please input the value for Left:");
+            test.Left = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please input the value for Right");
+            test.Right = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What calculation would you like to perform? Please enter a number:" +
+                "1-Addition" +
+                "2-Subtraction" +
+                "3-Multiplication" +
+                "4-Left Power" +
+                "5-Right Power");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+    }
+}
